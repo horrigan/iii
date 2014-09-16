788283f6d73e036b8467b76b159f99185e481920
unknown <denis.azaare@wsb-082.icx.local>
2014-09-16 13:16:01 +0300

    no message

--- app/js/controllers/edit-bug-ctrl.js
+++ app/js/controllers/edit-bug-ctrl.js
@@ -6,10 +6,10 @@ app.controller('BugDetailCtrl', ['$scope', '$stateParams', '$http', '$resource',
         butToEdit.$promise.then(
             function (result) {
                 $scope.edit_bug = result[0];
-               // $scope.edit = function (edit_bug) {
+                $scope.edit = function (edit_bug) {
                   // var editted_bug = bugResourceAll.update({q: '{' + '"_id":' + '"' + result[0]._id['$oid'] + '"'+ '}'},edit_bug);
-                  // var editted_bug = bugResourceEdit.update({ '':result[0]._id['$oid']},edit_bug)
-               // };
+                   var editted_bug = bugResourceEdit.update({ '':result[0]._id['$oid']},edit_bug)
+                };
             }
         );
 
@@ -17,21 +17,22 @@ app.controller('BugDetailCtrl', ['$scope', '$stateParams', '$http', '$resource',
             //var editted_bug = bugResourceAll.update({q: '{' + '"id":' + id + '' + '}'},edit_bug);
            // var editted = bugResourceEdit.update(edit_bug)
        // };
-
+        this.comments = {};
         $scope.addReview = function (ticket) {
-            $scope.comments = {};
-            console.log(ticket);
             var commentData = {
-                comment_author: $scope.ticket.comment_author,
-                comment_body: $scope.ticket.comment,
-                comment_time: $scope.updateDate
+                comment_author: $scope.ticket.comment_author || '',
+                comment: $scope.ticket.comment || '',
+                comment_time: $scope.updateDate || ''
             };
-            this.comments.push(ticket);
+            $scope.edit_bug.comments.push(commentData);
+            this.ticket = {};
+            /*
             var sendedData = {
                 author: commentData.comment_author,
                 comment: commentData.comment_body,
                 date: commentData.comment_time
             };
+            */
         };
 
 
