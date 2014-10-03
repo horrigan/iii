465e9333438af51d8bf5960804286dd986d9ef95
unknown <denis.azaare@wsb-082.icx.local>
2014-10-03 09:57:22 +0300

    -declarative sort, hide

--- app/js/controllers/table-ctrl.js
+++ app/js/controllers/table-ctrl.js
@@ -1,3 +1,9 @@
 app.controller('TableCtrl', function ($scope, tickets) {
-   $scope.tickets = tickets;
+    $scope.tickets = tickets;
+    var columnsHeaders = [];
+    angular.forEach($scope.tickets[0], function (value, key) {
+        this.push(key);
+    }, columnsHeaders);
+    $scope.columnsHeaders = columnsHeaders.sort();
+    $scope.templateTd = '<div class="newclasss"></div>'
 });
--- app/js/directives/bt-table-view.js
+++ app/js/directives/bt-table-view.js
@@ -1,36 +1,26 @@
-app.directive('btTableView', function ($filter) {
-    /*'<td><a ui-sref="edit-ticket({id: ticket._id})">{{ticket.id}}</a></td>' +
-     '<td>{{ticket.status}}</td>' +
-     '<td>{{ticket.type}}</td>' +
-     '<td>{{ticket.assignee}}</td>' +
-     '<td>{{ticket.createdDate | date: "dd/MM/yyyy"}}</td>' +*/
-    /* scope.columns = [];
-     angular.forEach(scope.items, function(value, key) {
-     console.log('/')
-     angular.forEach(value, function(value, key){
-     console.log(value, key)
-     this.push(value)
-     })
-     }, scope.columns);*/
+app.directive('btTableView', function () {
     return {
         restrict: 'EA',
         scope: {
             items: '=data',
-            temp:'=temp'
+            th:'=th'
         },
         template: '<div>' +
             '<table>' +
                 '<thead>' +
+                    '<th ng-repeat="(i,header) in th">' +
+                        '<input type="checkbox" ng-click="hideColumns(columnName)">{{header}}' +
+                    '</th>' +
                     '<tr>' +
-                        '<th ng-repeat="header in columnsHeaders">' +
+                        '<th ng-repeat="header in th" >' +
                             '<a ng-click="reverse=!reverse;order(header,reverse)">{{header}}</a>' +
                         '</th>' +
                     '</tr>' +
                 '</thead>' +
                 '<tbody>' +
-                '<tr ng-repeat="ticket in items | filter: paginate  track by $index">' +
-                    '<td ng-repeat="cell in ticket track by $index">' +
-                        '<div temp="ticket">{{cell}}</div>' +
+                '<tr ng-repeat="(i, ticket) in items | filter: paginate | orderBy:header:reverseSort track by $index" >' +
+                    '<td ng-repeat="cell in ticket track by $index" >' +
+                        '<div >{{cell}}</div>' +
                     '<td>' +
                 '</tr>' +
                 '</tbody>' +
@@ -42,39 +32,30 @@ app.directive('btTableView', function ($filter) {
             '  </div>',
         transclude: true,
         link: function (scope, element, attrs) {
-            /*console.log('attr',attrs);
-            console.log('element',element);
-            console.log(scope.items);*/
             scope.orderByField = 'firstName';
             scope.reverseSort = false;
-            var columnsHeaders = [];
-            angular.forEach(scope.items[0], function(value, key) {
-                this.push(key);
-            }, columnsHeaders);
-            scope.columnsHeaders = columnsHeaders.sort();
-
-            var orderBy = $filter('orderBy');
             scope.order = function (predicate, reverse) {
-                scope.items = orderBy(scope.items, predicate, reverse);
+                scope.header = predicate;
+                scope.reverseSort = reverse
             };
             scope.order('-id', false);
-
             scope.totalItems = scope.items.length;
             scope.currentPage = 1;
             scope.numPerPage = 5;
             scope.setPage = function (pageNo) {
                 scope.currentPage = pageNo;
             };
+            //ng-show="(checkValue == {{i}})"
+            scope.hideColumns = function(columnName){
+                    scope.checkValue = columnName
+            };
             scope.paginate = function (value) {
                 var begin, end, index;
                 begin = (scope.currentPage - 1) * scope.numPerPage;
                 end = begin + scope.numPerPage;
                 index = scope.items.indexOf(value);
-
                 return (begin <= index && index < end);
             };
-
-
         }
     }
 });
